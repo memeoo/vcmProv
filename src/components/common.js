e1adc9dd23d8bc9216b7470dfffdda5c3ce40d2f
memeSong <meme@memeSongui-MacBook-Pro.local>
2019-07-28 14:15:52 +0900

    Server URL Change

--- src/components/common.js
+++ src/components/common.js
@@ -1,5 +1,6 @@
 const comm = {
-    SERVER_URL : "http://ec2-13-209-19-115.ap-northeast-2.compute.amazonaws.com",
+    // SERVER_URL : "http://ec2-13-209-19-115.ap-northeast-2.compute.amazonaws.com",
+    SERVER_URL : "http://ec2-13-125-232-40.ap-northeast-2.compute.amazonaws.com",
     SERVER_PORT :  ":3000"
 
     // SERVER_URL : "localhost",
